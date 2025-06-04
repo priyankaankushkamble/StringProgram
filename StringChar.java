602aa0985a4a15d829e3466127fb634c4eedaca5
PriyankaKamble <priyankakamble18042003@gmail.com>
2025-06-04 19:52:16 +0530

    Add files via upload

---
+++ StringChar.java
@@ -0,0 +1,9 @@
+public class StringChar
+{
+	public static void main(String x[])
+	{
+		char ch[]=new char[]{'a','b','c','d','e'};
+		String s=new String(ch);
+		System.out.println(s);
+	}
+}
---
+++ StringData.java
@@ -0,0 +1,8 @@
+public class StringData
+{
+	public static void main(String x[])
+	{
+		String s=new String("Good");
+		System.out.println(s);
+	}
+}
