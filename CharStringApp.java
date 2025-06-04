e586b0bcc68889b8b79d5c0b2f5b4b875d1de1c3
PriyankaKamble <priyankakamble18042003@gmail.com>
2025-06-04 19:50:53 +0530

    Add files via upload

---
+++ CharStringApp.java
@@ -0,0 +1,9 @@
+public class CharStringApp
+{
+	public static void main(String x[])
+	{
+		char ch[]=new char[]{'g','o','o','d',' ','m','o','r','n','i','n','g','i','n','d','i','a'};
+		String s=new String(ch ,5,7);
+		System.out.println(s);
+	}
+}
